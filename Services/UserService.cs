834d5fe85692c1c45342fd5c0d619f6df6ddd43e
Girthstorm <ayounathan05@gmail.com>
2024-05-02 11:20:32 -0700

    Some user stuff

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,199 +2,206 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using TaskTrackerBackend.Models;
 using TaskTrackerBackend.Models.DTO;
 using TaskTrackerBackend.Services.Context;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
-namespace TaskTrackerBackend.Services;
-
+namespace TaskTrackerBackend.Services
+{
     public class UserService : ControllerBase
     {
+        
 
         private readonly DataContext _context;
 
-        public UserService(DataContext context){
+        public UserService(DataContext context)
+        {
             _context = context;
         }
 
-
-        public bool DoesUserExist(string Username){
-            // check if username exist
-            // if 1 item matches then return the item
-            // if no item matches, then return null
-            
-            return _context.UserInfo.SingleOrDefault(user => user.Username == Username) != null;
+        public bool DoesUserExist(string Username)
+        {
+            return _context.UserInfo.SingleOrDefault(User => User.Username == Username) != null;
         }
 
-        public bool AddUser(CreateAccountDTO UserToAdd){
+        public string GenerateImgID()
+        {
+            string[] abcArr = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            Random random = new Random();
 
-            // if user already exists
-            // if does not exist create account
-            // else return false
+            if (random.Next(0, 2)== 0)
+            {
+
+            }
+
+            int firstLetterIndex  = random.Next(0, abcArr.Length);
+
+            string firstLetter = abcArr[firstLetterIndex];
+
+            string remainingDigits = "";
+                for (int i = 0; i < 4; i++)
+                {
+                    if (random.Next(0, 2)== 0)
+                    {
+                        int i 
+                    }
+                    
+
+            return imgID;
+
+        } }
+
+
+        public bool CreateUser(CreateAccountDTO UserToAdd)
+        {
             bool result = false;
 
-            // if user doesn't exist, add user
-            if(!DoesUserExist(UserToAdd.Username)){
+            if(!DoesUserExist(UserToAdd.Username))
+            {
                 UserModel newUser = new UserModel();
 
                 var hashPassword = HashPassword(UserToAdd.Password);
+                
+                    newUser.ID = UserToAdd.ID;
 
-                newUser.ID = UserToAdd.ID;
-                newUser.Username = UserToAdd.Username;
-                newUser.Salt = hashPassword.Salt;
-                newUser.Hash = hashPassword.Hash;
+                    newUser.Username = UserToAdd.Username;   
+                    newUser.Password = "Cant see it";
 
-                _context.Add(newUser);
 
-                // save into database, return of number of entries written into database
-                result = _context.SaveChanges() != 0;
+                    newUser.Salt = hashPassword.Salt;
+                    newUser.Hash = hashPassword.Hash;  
+                    newUser.AccountCreated = true;
+                    newUser.BoardInfo = new List<BoardModel>();
 
+                    _context.Add(newUser);
+
+                    result = _context.SaveChanges() != 0;  
+                    result = true;    
             }
 
             return result;
         }
 
-        // HashedPassword = H(Salt + Password)
 
-        public PasswordDTO HashPassword(string password){
+        public PasswordDTO HashPassword(string Password)
+        {
 
-            PasswordDTO newHashPassword = new PasswordDTO();
+            PasswordDTO NewHashPassword = new PasswordDTO();
 
-            // create a byte array using 64 bytes of salt.
-            byte[] SaltByte = new byte[64];
-
-            // this is our randomizer
+            byte[] SaltBytes = new byte[64];
+            
             RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
 
-            // take our SaltByte and make sure it contains no zero's. Making it more secure.
-            provider.GetNonZeroBytes(SaltByte);
+            provider.GetNonZeroBytes(SaltBytes);
 
-            // converting our salt into a string
-            string salt = Convert.ToBase64String(SaltByte);
+            string salt = Convert.ToBase64String(SaltBytes);
 
+            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(Password, SaltBytes, 1000);
 
-            // encode our password with salt into our hash
-            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltByte, 10000);
-
-            // convert the resulting byte array as a string of 256 bytes
             string hash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
 
-            // we can save our hash and salt into our passwordDTO
-            newHashPassword.Salt = salt;
-            newHashPassword.Hash = hash;
+            NewHashPassword.Salt = salt;
+            NewHashPassword.Hash = hash;
 
-
-            return newHashPassword;
-        }
-        
-
-        // verify users password
-        public bool VerifyUsersPassword(string? password, string? storedHash, string? storedSalt){
-
-            // encode salt back in the original byte array
-
-            byte[] SaltBytes = Convert.FromBase64String(storedSalt);
-
-            // repeat process of taking password entered and hashing it again
-            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltBytes, 10000);
-
-            // RFC289 object, retrieve the 256 bytes of hash, convert those into a string, assign the result to the newhash
-            string newhash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
-
-            return newhash == storedHash;
+            return NewHashPassword;
 
         }
 
-        public IActionResult Login(LoginDTO User){
+        public bool VerifyPassword(string Password, string StoredHash, string StoredSalt)
+        {
+            byte[] SaltBytes = Convert.FromBase64String(StoredSalt);
 
+            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(Password, SaltBytes, 1000);
+
+            string NewHash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+
+            return NewHash == StoredHash;
+
+        }
+
+        public IActionResult Login(LoginDTO User)
+        {
             IActionResult Result = Unauthorized();
 
-            // check if user exists
-            if(DoesUserExist(User.Username)){
-                // If true, continue with authentication
-                // if true, store our user object
-                UserModel founderUser = GetUserByUsername(User.Username);
+            if(DoesUserExist(User.Username))
+            {
 
-                // check if password is correct
-                if(VerifyUsersPassword(User.Password, founderUser.Hash, founderUser.Salt)){
+                UserModel foundUser = GetUserByUsername(User.Username);
 
-                    // anyone with this code can access the login
+                if(VerifyPassword(User.Password, foundUser.Hash, foundUser.Salt)){
+
                     var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-
-                    // sign in credentials
+                    
                     var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
-                    // generate new token and log user out after 30 mins
-                    var tokeOptions = new JwtSecurityToken(
+                    var tokenOptions = new JwtSecurityToken(
                         issuer: "http://localhost:5000",
                         audience: "http://localhost:5000",
-                        claims: new List<Claim>(), // Claims can be added here if needed
-                        expires: DateTime.Now.AddMinutes(30), // Set token expiration time (e.g., 30 minutes)
-                        signingCredentials: signinCredentials // Set signing credentials
+                        claims: new List<Claim>(),
+                        expires: DateTime.Now.AddMinutes(30),
+                        signingCredentials: signinCredentials
                     );
+                    
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
 
-                    // Generate JWT token as a string
-                    var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
 
-                    // return JWT token through http response with status code 200
-                    Result = Ok(new { Token = tokenString });
+                    Result = Ok(new { Token = tokenString});
                 }
-
-                // Token:
-                    // awdjiawdjoaw. = header
-                    // rksigsdfghsi. = Payload: contains claims such as expiration time
-                    // lfkdjsdfkjgk. = signature: encrypts and combines header and payload
-
             }
             return Result;
         }
-        
-        public UserModel GetUserByUsername(string username){
+
+        public UserModel GetUserByUsername(string username)
+        {
             return _context.UserInfo.SingleOrDefault(user => user.Username == username);
         }
 
-        public bool UpdateUser(UserModel userToUpdate){
-            _context.Update<UserModel>(userToUpdate);
-            return _context.SaveChanges() != 0;
+        public List<BoardModel> GetBaordById(string BoardID)
+        {
+            return _context.BoardInfo.Where(board => board.BoardID == BoardID).ToList();
         }
 
-        public bool UpdateUsername(int id, string username){
-            // sending over just the id and username
-            // we have to get the object to be updated
 
+        public bool UpdateUser(UserModel userToUpdate)
+        {
+            _context.Update<UserModel>(userToUpdate);
+            return _context.SaveChanges() !=0 ;
+        }
+
+        public bool UpdateUsername(int id, string username)
+        {
             UserModel foundUser = GetUserById(id);
 
             bool result = false;
-
-            if(foundUser != null){
-                // a user was found
-                // update founderUser object
-
+             if(foundUser != null)
+             {
                 foundUser.Username = username;
                 _context.Update<UserModel>(foundUser);
-                result = _context.SaveChanges() != 0;
-            }
-
-            return result;
+                result = _context.SaveChanges() !=0 ;
+             }
+             return result;
 
         }
 
-        public UserModel GetUserById(int id){
-            return _context.UserInfo.SingleOrDefault(userID => userID.ID == id);
+        public UserModel GetUserById(int id)
+        {
+            return _context.UserInfo.SingleOrDefault(user => user.ID == id);
         }
 
-        public bool DeleteUser(string userToDelete){
-            // we are only sending over the username
-            // if username found found, delete user
+        
+        public bool DeleteUser(string userToDelete)
+        {
+            
 
             UserModel foundUser = GetUserByUsername(userToDelete);
 
             bool result = false;
 
-            if(foundUser != null){
-                // user was found
+            if (foundUser != null)
+            {
+               
 
                 _context.Remove<UserModel>(foundUser);
                 result = _context.SaveChanges() != 0;
@@ -203,17 +210,24 @@ namespace TaskTrackerBackend.Services;
             return result;
         }
 
-        public UserIdDTO GetUserIdDTOByUsername(string username){
+        public UserIdDTO GetUserIdDTOByUsername(string username)
+        {
             UserIdDTO UserInfo = new UserIdDTO();
 
-            // query through database to find the user
+            
             UserModel foundUser = _context.UserInfo.SingleOrDefault(user => user.Username == username);
 
             UserInfo.UserId = foundUser.ID;
 
+           
             UserInfo.PublisherName = foundUser.Username;
 
             return UserInfo;
         }
 
+
+
+
+
     }
+}
